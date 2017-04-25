27fd5b7df3fc1f3a0c5853eeb9bc8f237efd4311
Davion Cottle <Davion Cottle@CAPDAVIONC>
2017-04-25 17:08:58 -0500

    timer and file watcher expanded

---
+++ TestWatchDog/TestWatchDogService/Service1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace TestWatchDogService
+{
+    partial class TestWatchDogService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            eventLog1 = new System.Diagnostics.EventLog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(eventLog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            // 
+            // TestWatchDogService
+            // 
+            this.ServiceName = "TestWatchDogService";
+            ((System.ComponentModel.ISupportInitialize)(eventLog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+
+        }
+
+
+
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
---
+++ TestWatchDog/TestWatchDogService/Service1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+using System.Timers;
+using System.Threading;
+
+namespace TestWatchDogService
+{
+    public partial class TestWatchDogService : ServiceBase
+    {
+        string WatchPath1 = ConfigurationManager.AppSettings["WatchPath1"];
+        string WatchPath2 = ConfigurationManager.AppSettings["WatchPath2"];
+
+        private static DateTime lastfilefound = DateTime.Now;
+        private static System.Timers.Timer checkFileTimer;
+        public TestWatchDogService()
+        {
+            InitializeComponent();
+
+            checkFileTimer = new System.Timers.Timer(30000);
+
+            if (!System.Diagnostics.EventLog.SourceExists("TestSource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "TestSource", "TestLog");
+            }
+            eventLog1.Source = "TestSource";
+            eventLog1.Log = "TestLog";
+
+            //fileSystemWatcher1.Created += fileSystemWatcher1_Created;
+            fileSystemWatcher1.Changed += fileSystemWatcher1_Changed;
+            fileSystemWatcher1.Created += fileSystemWatcher1_Changed;
+            fileSystemWatcher1.Deleted += fileSystemWatcher1_Changed;
+            fileSystemWatcher1.Renamed += fileSystemWatcher1_Renamed;
+
+            checkFileTimer.Elapsed += new ElapsedEventHandler(checkFiles);
+        }
+
+        private static void checkFiles(object source, ElapsedEventArgs e)
+        {
+            System.IO.File.Create("C:\\test\\checkfiles.txt");
+            if (DateTime.Now - lastfilefound > TimeSpan.FromMinutes(1) )
+            {
+                System.IO.File.Create("C:\\test\\test.txt");
+            }
+        }
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Watch Dog test start 4/25 #1");
+            try
+            {
+                fileSystemWatcher1.Path = WatchPath1;
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Watch Dog test OnStop");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("watch dog In OnContinue.");
+        }
+
+
+        void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            try
+            {
+                eventLog1.WriteEntry("File system has changed");
+                lastfilefound = DateTime.Now;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        void fileSystemWatcher1_Renamed(object sender, System.IO.RenamedEventArgs e)
+        {
+            try
+            {
+                eventLog1.WriteEntry("File name has changed");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
